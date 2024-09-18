006a8dda7337d8519d8986eeb5f5a3bb438fcb12
TakanoriShima <quark2galaxy@gmail.com>
2024-09-18 04:59:25 +0000

    PHP/Laravel 16 投稿したニュースを更新/削除しよう

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// 以下の1行を追記することで、Profile Modelが扱えるようになる
+use App\Models\Profile;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+        // 以下を追記
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        // 追記ここまで
+        return redirect('admin/profile/create');
+    }
+
+    public function edit(Request $request)
+    {
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+
+        return redirect('admin/profile/edit?id=' . $request->id);
+    }
+}
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.profile')
+@section('title', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集画面</h2>
+                <form action="{{ route('admin.profile.update') }}" method="post">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">名前</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender }}">
+                        </div>
+                    </div> 
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                        </div>
+                    </div>                                       
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                    @csrf
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>                
+            </div>
+        </div>
+    </div>
+@endsection
